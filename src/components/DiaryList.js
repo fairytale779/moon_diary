ee09af8960470fe589bf5722fd59f1f9018ab7b3
홍동화 <hhhhong44@gmail.com>
2023-03-07 21:14:38 +0900

    3

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,8 +1,102 @@
-const DiaryList = ({ diaryList }) => {
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import MyButton from "./MyButton";
+
+const sortOptionList = [
+  { value: "latest", name: "최신 순" },
+  { value: "oldest", name: "오래된 순" },
+];
+
+const filterOptionList = [
+  { value: "all", name: "전부 다" },
+  { value: "good", name: "좋은 감정만" },
+  { value: "bad", name: "나쁜 감정만" },
+];
+
+const ControlMenu = ({ value, onChange, optionList }) => {
   return (
-    <div>
-      {diaryList.map((it) => (
-        <div key={it.id}>{it.content}</div>
+    <select
+      className="ControlMenu"
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+    >
+      {optionList.map((it, idx) => (
+        <option key={idx} value={it.value}>
+          {it.name}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+// value : select가 어떠한 역할인지에 대한 정보
+// onChange : select가 선택하는게 변화하였을 때 바꿀 기능을 할 함수
+// optionList : <select> 안에 들어갈 옵션 (최신 순, 오래된 순/)
+
+const DiaryList = ({ diaryList }) => {
+  const navigate = useNavigate();
+  const [sortType, setSortType] = useState("latest");
+  const [filter, setFilter] = useState("all");
+
+  // sort()를 쓰면 원본배열이 변화하기 때문에 배열을 copy. 깊은 복사를 해준다
+  // JSON.stringify(diaryList) 는 diaryList를 문자화시킴
+  // 그걸 parse로 실행시키면 다시 배열로 됨 그래서 배열 자체 모양은 건드려지지 않음
+  const getProcessDiaryList = () => {
+    const filterCallback = (item) => {
+      if (filter === "good") {
+        return parseInt(item.emotion) <= 2;
+      } else if (filter === "bad") {
+        return parseInt(item.emotion) >= 4;
+      }
+    };
+
+    const compare = (a, b) => {
+      if (sortType === "latest") {
+        return parseInt(b.date) - parseInt(a.date);
+        // 문자열이 들어올 수도 있기 때문에 parseInt를 해주어야 함 (숫자로 바꾸어 줌)
+      } else {
+        return parseInt(a.date) - parseInt(b.date);
+      }
+    };
+
+    const copyList = JSON.parse(JSON.stringify(diaryList));
+    const filteredList =
+      filter === "all" ? copyList : copyList.filter((it) => filterCallback(it));
+
+    const sortedList = filteredList.sort(compare);
+    return sortedList;
+  };
+
+  return (
+    <div className="DiaryList">
+      <div className="menu_wrap">
+        <div className="left_col">
+          <ControlMenu
+            value={sortType}
+            onChange={setSortType}
+            optionList={sortOptionList}
+          />
+          <ControlMenu
+            value={filter}
+            onChange={setFilter}
+            optionList={filterOptionList}
+          />
+        </div>
+        <div className="right_col">
+          <MyButton
+            type={"positive"}
+            text={"새로운 일기쓰기"}
+            onClick={() => {
+              navigate("/new");
+            }}
+          />
+        </div>
+      </div>
+
+      {getProcessDiaryList().map((it) => (
+        <div key={it.id}>
+          {it.content} 감정:{it.emotion}
+        </div>
       ))}
     </div>
   );
